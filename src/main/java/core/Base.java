083b7121d68b3f06dacc214ce10a155af119a5b4
Nasir Saeedy <nasir.saeedy261@gmail.com>
2021-10-28 19:47:47 -0400

    commit

--- src/main/java/core/Base.java
+++ src/main/java/core/Base.java
@@ -6,7 +6,6 @@ import java.io.FileReader;
 import java.io.IOException;
 import java.util.Properties;
 import java.util.concurrent.TimeUnit;
-
 import org.apache.log4j.Logger;
 import org.apache.log4j.PropertyConfigurator;
 import org.openqa.selenium.WebDriver;
